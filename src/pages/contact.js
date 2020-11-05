f4d4867f8aeff4b27d3af41516efd398e9cd63bf
vismay03 <vismaykatharani03@gmail.com>
2020-11-06 04:36:04 +0530

    update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Circle} from 'react-preloaders';
+import Slide from 'react-reveal/Slide';
 import axios from 'axios';
 export default class contact extends Component {
     constructor(props) {
@@ -62,18 +62,20 @@ export default class contact extends Component {
     render() {
         return (
             <main className="flex flex-col w-full"  >
-                <Circle time={2000} customLoading={this.state.loading} />
+               <Slide left>
+                   
+               
                 <div className="aboutImage text-white flex justify-center pl-16 flex-col">
                     <h3 className="text-4xl font-semibold uppercase text-oswald " >Contact us</h3>
                     <p className="text-1xl text-open-sans">Established in the year 2016 at Morbi, Gujarat</p>
-                </div>
+                </div></Slide>
                 <div className="w-full flex flex-col py-10 justify-around items-center border"> 
                     <h2 className=" text-2xl py-4 text-center uppercase  text-oswald text-darkorange-600">Contact us</h2>
                     <form className="shadow-md  flex flex-col w-64 p-3 content-evenly border" onSubmit={this.submitEmail}>
                         <input className="border m-2 py-1 px-2 placeholder-gray-500" value={this.state.name} onChange={this.onNameChange} type="text" placeholder="Name" />
                         <input className="border m-2 py-1 px-2 placeholder-gray-500" onChange={this.onEmailChange} type="text" placeholder="Email" />
                         <textarea className="border m-2 py-1 px-2 placeholder-gray-500" onChange={this.onMsgChange} > </textarea>
-                        <button className="border m-2 hover:bg-white border-gray-500 bg-gray-300 py-1 px-2 " type="submit">delete</button>
+                        <button className="border m-2 hover:bg-white border-gray-500 bg-gray-300 py-1 px-2 " type="submit">submit</button>
                     </form>
                 </div>
             </main>
