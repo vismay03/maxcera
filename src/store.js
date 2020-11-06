dcba2843a8f52f784a73f0cab2f0562d63bddf60
vismay03 <vismaykatharani03@gmail.com>
2020-11-06 13:42:25 +0530

    update

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+const initialState = {}
+const reducer = {}
+
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
+    reducer,
+    initialState,
   composeEnhancer(applyMiddleware(thunk))
 );
 
