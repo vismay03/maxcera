3c5e519bdf8252f1b5a3ac5339dfcffdbc52ec51
vismay03 <vismaykatharani03@gmail.com>
2020-11-07 11:42:39 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
+
 const cors = require('cors');
 const mongoose = require('mongoose');
 const express = require('express');
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI || "mongodb+srv://Maxcera:Maxcer%40@cluster0.y1fcd.mongodb.net/Maxcera?retryWrites=true&w=majority";
 const app = express();
 const productRoute = require('./routes/productRoute');
 const path = require('path');
 const loginRoute = require('./routes/loginRoute');
 
+
+require('dotenv').config({  path: "env" } );
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +18,11 @@ mongoose.connect(uri,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
+})
+.then(()=>{
+  console.log("conected to mongodb");
+}).catch(error => {
+  console.log("mongo error",error);
 })
 
 
